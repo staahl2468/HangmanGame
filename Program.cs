a5abd9108e0a34690c014725ba8c6286753c6616
staah <staah@DESKTOP-MTJ7CTS>
2022-04-22 09:50:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace HangmanGame
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            char[] Wordarray = RandomWord();
+            char[] Guessed = MakeGuessed(Wordarray);
+            int Mistakecounter = 0;
+            int Win = 0;
+            do
+            {
+                char Currentguess = TakeGuess();
+                Guessed = Compare(Currentguess, Wordarray, Guessed);
+                Console.WriteLine(Guessed);
+                Mistakecounter = MistakeCounter(Currentguess, Wordarray, Mistakecounter);
+                Win = CheckForWin(Guessed, Wordarray, Win);
+            }while(Mistakecounter < 10 && Win == 0);
+            if (Win != 0)
+            {
+                Console.WriteLine("YOU WIN!!!");
+            }
+            else
+            {
+                Console.WriteLine("GAME OVER");
+            }
+        }
+
+        public static char[] RandomWord()
+        {
+            string word = "test";
+            char[] wordarray = new char [word.Length];
+            for (int i = 0; i < word.Length; i++)
+            {
+                wordarray[i] = word[i];
+            }
+            return wordarray;
+        }
+
+        public static char[] MakeGuessed(char[] word)
+        {
+            char[] guessed = new char [word.Length];
+            for (int i = 0; i < word.Length; i++)
+            {
+                guessed[i] = Convert.ToChar("_");
+            }
+            return guessed;
+        }
+
+        public static char TakeGuess()
+        {
+            Console.WriteLine("Take a guess");
+            return Convert.ToChar(Console.ReadLine());
+        }
+
+        public static char[] Compare(char guess, char[] word, char[] guessed)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (guess == word[i])
+                {
+                    guessed[i] = guess;
+                }
+            }
+            return guessed;
+        }
+
+        public static int MistakeCounter(char guess, char[] word, int counter)
+        {
+            if (!word.Contains(guess))
+            {
+                return counter += 1;
+            }
+            else
+            {
+                return counter;
+            }
+        }
+
+        public static int CheckForWin(char[] guessed, char[] word, int win)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (guessed[i] == word[i])
+                {
+                    win += 1;
+                }
+            }
+            if (win == word.Length)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
